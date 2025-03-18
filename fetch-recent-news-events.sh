92a3831fe5d56f27e5cf24cd7efa732cfa8d8e69
IsaacVerm <isaacverm@gmail.com>
2025-03-18 23:15:34 +0100

    merge newly fetched events with events already saved

--- fetch-recent-news-events.sh
+++ fetch-recent-news-events.sh
@@ -3,3 +3,9 @@ curl https://www.vrt.be/vrtnws/_next/data/lOVEAve2b6oRA8in28B4v/nl/net-binnen.js
     # some objects are not really about events but just ads, these have no uri and can be removed
     jq -c ".pageProps.data.compositions[0].compositions[] | {title: .title.text, timestamp: .metadata[0].timestamp, tag: .tag.text, uri: .action.uri} | select(.uri != null)" \
     > recent-news-events.json
+
+# https://jqlang.org/manual/#invoking-jq
+# -s (slurp) reads all files into a single array
+# https://jqlang.org/manual/#group_by
+# if there's a duplicate uri, I just keep the first object
+jq . news-events.json recent-news-events.json | jq -s 'group_by(.uri)[] | .[0]' | jq -c . > news-events.json

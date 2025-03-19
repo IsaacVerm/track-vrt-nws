7bebef437a340b5597f698e7766dd4205cc57cf1
IsaacVerm <isaacverm@gmail.com>
2025-03-19 11:40:29 +0100

    update definition new event: new title is considered new event

--- fetch-recent-news-events.sh
+++ fetch-recent-news-events.sh
@@ -9,10 +9,16 @@ curl https://www.vrt.be/vrtnws/_next/data/u6iWbVYnh9kHIkeff4lxk/nl/net-binnen.js
 # now I compare recent-news-events.json with main-events
 # I want to know which events are new
 # these events are saved to a file called new-events.json
+# to determine whether an event is new I could either look at the url or the article title
+# the url (called uri here) always remains the same, even if the article title changes
+# however, I find these changes to the article titles themselves interesting
+# so whenever the title of an article changes, I consider it a new event
+# even if it still refers to the same article
+# so in a sense new-events.json contains duplicate events but with different titles
 # -n (--null-input) is required because the data is loaded using --slurpfile, not from standard input
 # so nothing like cat main-events.json | ...
 jq -c -n --slurpfile main main-events.json --slurpfile recent recent-events.json \
-   '$recent | map(select(.uri as $uri | $main | map(.uri) | index($uri) | not)) | .[]' \
+   '$recent | map(select(.title as $title | $main | map(.title) | index($title) | not)) | .[]' \
    > new-events.json
 
 # concatenate main-events.json and new-events.json and save the result to main-events.json

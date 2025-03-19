1df2a61ae997156ef01990c55ee59e12c8a44a54
IsaacVerm <isaacverm@gmail.com>
2025-03-19 13:46:13 +0100

    use jsonl as file format

--- fetch-recent-news-events.sh
+++ fetch-recent-news-events.sh
@@ -11,22 +11,26 @@ echo "Successfully retrieved dynamic URL part: $DYNAMIC_URL_PART"
 curl "https://www.vrt.be/vrtnws/_next/data/${DYNAMIC_URL_PART}/nl/net-binnen.json" | \
     # some objects are not really about events but just ads, these have no uri and can be removed
     jq -c ".pageProps.data.compositions[0].compositions[] | {title: .title.text, timestamp: .metadata[0].timestamp, tag: .tag.text, uri: .action.uri} | select(.uri != null)" \
-    > recent-events.json
+    > recent-events.jsonl
 
-# now I compare recent-news-events.json with main-events
+# now I compare recent-news-events.jsonl with main-events
 # I want to know which events are new
-# these events are saved to a file called new-events.json
+# these events are saved to a file called new-events.jsonl
 # to determine whether an event is new I could either look at the url or the article title
 # the url (called uri here) always remains the same, even if the article title changes
 # however, I find these changes to the article titles themselves interesting
 # so whenever the title of an article changes, I consider it a new event
 # even if it still refers to the same article
-# so in a sense new-events.json contains duplicate events but with different titles
+# so in a sense new-events.jsonl contains duplicate events but with different titles
 # -n (--null-input) is required because the data is loaded using --slurpfile, not from standard input
-# so nothing like cat main-events.json | ...
-jq -c -n --slurpfile main main-events.json --slurpfile recent recent-events.json \
+# so nothing like cat main-events.jsonl | ...
+jq -c -n --slurpfile main main-events.jsonl --slurpfile recent recent-events.jsonl \
    '$recent | map(select(.title as $title | $main | map(.title) | index($title) | not)) | .[]' \
-   > new-events.json
+   > new-events.jsonl
 
-# concatenate main-events.json and new-events.json and save the result to main-events.json
-cat new-events.json >> main-events.json
+# append new events to main events (in JSONL format)
+cat new-events.jsonl >> main-events.jsonl
+
+# have main events in JSON format as well next to JSONL format
+# this is required to display the data in Datasette Lite
+jq -s '.' main-events.jsonl > main-events.json
